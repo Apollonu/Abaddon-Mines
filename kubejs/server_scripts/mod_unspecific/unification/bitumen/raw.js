18db45bb673f5f09446c6d35b0b427ed807aebd3
Apollo <kingfrostalpha@gmail.com>
2025-09-03 19:07:42 +0000

    Add files via upload

---
+++ kubejs/server_scripts/mod_unspecific/unification/bitumen/raw.js
@@ -0,0 +1,49 @@
+//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
+//priority: 3
+
+//Recipe Event
+ServerEvents.recipes(event => {
+    
+    //Variables
+    let unified_individual = 'thermal:bitumen'
+    let unified_variants = [
+        'tfmg:bitumen'
+    ]
+
+    //Output Recipe Types
+    let output_recipe_types = [
+        
+    ]
+
+    //Input Recipe Types
+    let input_recipe_types = [
+        'create:mixing'
+    ]
+
+    //Custom Recipe Types
+    let custom_recipe_types = [
+        {
+            'type': 'create:compacting',
+            'heatRequirement': 'heated',
+            'ingredients': [{
+                'amount': 1000,
+                'fluid': 'thermal:heavy_oil',
+                'nbt': {}
+            }],
+            'results': [{'item': unified_individual}]
+        }
+    ]
+
+    //Other Recipe Types
+    
+
+    //Unify Items Function Push
+    unify_items (
+        event,
+        unified_individual,
+        unified_variants,
+        output_recipe_types,
+        input_recipe_types,
+        custom_recipe_types
+    )
+})
