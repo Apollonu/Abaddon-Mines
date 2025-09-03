aeac0825dde1c57163f87c17542e902902048e70
Apollo <kingfrostalpha@gmail.com>
2025-09-03 13:58:39 +0000

    Delete kubejs/server_scripts/mod_unspecific/progression/tier_1/string.js

--- kubejs/server_scripts/mod_unspecific/progression/tier_1/string.js
+++
@@ -1,8 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-
-//Recipe Event
-ServerEvents.recipes(event => {
-
-    //String Recipe
-    event.blasting('minecraft:string', 'htm:plant_string')
-})
