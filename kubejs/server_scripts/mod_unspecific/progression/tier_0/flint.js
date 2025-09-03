afe40ba991637d48083b39f1a53bc005c5fec6ab
Apollo <kingfrostalpha@gmail.com>
2025-09-03 12:38:36 +0000

    Delete kubejs/server_scripts/mod_unspecific/progression directory

--- kubejs/server_scripts/mod_unspecific/progression/tier_0/flint.js
+++
@@ -1,8 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-
-//Recipe Event
-ServerEvents.recipes(event => {
-    
-    //Flint Recipe
-    event.shapeless( 'minecraft:flint', ['minecraft:coarse_dirt', 'minecraft:coarse_dirt', 'minecraft:coarse_dirt'])
-})
--- kubejs/server_scripts/mod_unspecific/progression/tier_2/iron.js
+++
@@ -1,8 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-
-//Recipe Event
-ServerEvents.recipes(event => {
-	
-	//Iron Rod Recipe
-	event.remove({output: '#forge:rods/iron', type: 'minecraft:crafting_shaped'})
-})
