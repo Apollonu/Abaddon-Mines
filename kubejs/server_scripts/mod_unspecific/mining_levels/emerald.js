93cc19e280cdd5d83909d4ab5241254f00457711
Apollo <kingfrostalpha@gmail.com>
2025-09-03 19:49:38 +0000

    Add files via upload

---
+++ kubejs/server_scripts/mod_unspecific/mining_levels/emerald.js
@@ -0,0 +1,27 @@
+//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
+//priority: 1
+
+//Block Tag Event
+ServerEvents.tags('block', event => {
+	
+	//Variables
+	let material = 'emerald'
+
+	//Individual Blocks
+	let individual_blocks = [
+		'minecraft:deepslate_emerald_ore'
+	]
+
+	//Individual Tags
+	let individual_tags = [
+		
+	]
+
+	//Block Tags Function Push
+	block_tags (
+		event,
+		material,
+		individual_blocks,
+		individual_tags
+	)
+})
