456ed4804f19ad0210c95f70656b1857d1a68bea
Apollo <kingfrostalpha@gmail.com>
2025-09-08 17:51:45 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/progression/tier_1/emerald.js
+++ kubejs/server_scripts/mod_unspecific/progression/tier_1/emerald.js
@@ -21,15 +21,84 @@ ServerEvents.recipes(event => {
         '#forge:gems/emerald'
     )
 
-    //Melting Recipe
-    event.custom({
-        'type': 'tconstruct:melting',
-        'ingredient': {'item': 'minecraft:jukebox'},
-        'result': {
-            'amount': 100,
-            'fluid': 'tconstruct:molten_emerald'
-        },
-        'temperature': 1450,
-        'time': 83
+    //Custom Recipe Types
+    let custom_recipe_types = [
+        {
+            'type': 'tconstruct:melting',
+            'ingredient': {'item': 'minecraft:jukebox'},
+            'result': {
+                'amount': 100,
+                'fluid': 'tconstruct:molten_emerald'
+            },
+            'temperature': 1450,
+            'time': 83
+        }, {
+            'type': 'tconstruct:ore_melting',
+            'conditions': [{
+                'type': 'mantle:tag_combination_filled',
+                'match': [
+                    'forge:ores/emerald',
+                    'forge:ore_rates/dense'
+                ]
+            }],
+            'ingredient': {
+                'type': 'forge:intersection',
+                'children': [
+                    {'tag': 'forge:ores/emerald'},
+                    {'tag': 'forge:ore_rates/dense'}
+                ]
+            },
+            'rate': 'gem',
+            'result': {
+                'amount': 300,
+                'fluid': 'tconstruct:molten_emerald'
+            },
+            'temperature': 934,
+            'time': 289
+        }, {
+            'type': 'tconstruct:ore_melting',
+            'conditions': [{
+                'type': 'mantle:tag_combination_filled',
+                'ignore': 'tconstruct:non_singular_ore_rates',
+                'match': 'forge:ores/emerald'
+            }],
+            'ingredient': {
+                'type': 'forge:difference',
+                'base': {'tag': 'forge:ores/emerald'},
+                'subtracted': {'tag': 'tconstruct:non_singular_ore_rates'}
+            },
+            'rate': 'gem',
+            'result': {
+                'amount': 100,
+                'fluid': 'tconstruct:molten_emerald'
+            },
+            'temperature': 934,
+            'time': 161
+        }, {
+            'type': 'tconstruct:ore_melting',
+            'conditions': [{
+                'type': 'mantle:tag_combination_filled',
+                'match': [
+                    'forge:ores/emerald',
+                    'forge:ore_rates/sparse'
+                ]
+            }],
+            'ingredient': {
+                'type': 'forge:intersection',
+                'children': [
+                    {'tag': 'forge:ores/emerald'},
+                    {'tag': 'forge:ore_rates/sparse'}
+                ]
+            },
+            'rate': 'gem',
+            'result': {
+                'amount': 50,
+                'fluid': 'tconstruct:molten_emerald'
+            },
+            'temperature': 934,
+            'time': 96
+        }
+    ].forEach(type => {
+        event.custom(type)
     })
 })
