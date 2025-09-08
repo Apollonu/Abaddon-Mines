27ca64a2568b15f8d94d09dd06e161f242093dfa
Apollo <kingfrostalpha@gmail.com>
2025-09-08 13:24:07 +0000

    Add files via upload

--- kubejs/server_scripts/mod_specific/removals/advanced/thermal.js
+++ kubejs/server_scripts/mod_specific/removals/advanced/thermal.js
@@ -12,6 +12,7 @@ ServerEvents.tags('item', event => {
 	
 	//Individual Items
 	let individual_items = [
+		prefix + 'charcoal_block',
 		prefix + 'chiseled_slag',
 		prefix + 'cracked_slag_bricks',
 		prefix + 'coal_coke',
