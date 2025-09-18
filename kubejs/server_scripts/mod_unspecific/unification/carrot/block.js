4d2e6149cbe8974ab1d80c425c66a2ffc541ea20
Apollo <kingfrostalpha@gmail.com>
2025-09-18 15:49:35 +0000

    Add files via upload

---
+++ kubejs/server_scripts/mod_unspecific/unification/carrot/block.js
@@ -0,0 +1,41 @@
+//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
+//priority: 3
+
+//Recipe Event
+ServerEvents.recipes(event => {
+    
+    //Variables
+    let unified_individual = 'farmersdelight:carrot_crate'
+    let unified_variants = [
+        'thermal:carrot_block'
+    ]
+
+    //Output Recipe Types
+    let output_recipe_types = [
+        'thermal:press'
+    ]
+    
+    //Input Recipe Types
+    let input_recipe_types = [
+        'thermal:gourmand_fuel',
+        'thermal:press'
+    ]
+
+    //Custom Recipe Types
+    let custom_recipe_types = [
+
+    ]
+
+    //Other Recipe Types
+    
+
+    //Unify Items Function Push
+    unify_items (
+        event,
+        unified_individual,
+        unified_variants,
+        output_recipe_types,
+        input_recipe_types,
+        custom_recipe_types
+    )
+})
