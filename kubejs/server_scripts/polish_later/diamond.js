b1eba2736a8e0040d5a465b87bdf020d0a7aa404
Apollo <kingfrostalpha@gmail.com>
2025-09-21 01:05:58 +0000

    Add files via upload

---
+++ kubejs/server_scripts/polish_later/diamond.js
@@ -0,0 +1,13 @@
+//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
+
+//Recipe Event
+ServerEvents.recipes(event => {
+    
+    //Barrel Recipe
+    event.remove({output: 'immersiveengineering:drillhead_steel'})
+    event.shaped('immersiveengineering:drillhead_steel', ['  C', 'AA ', 'BA '], {
+        A: '#forge:ingots/steel',
+        B: '#forge:storage_blocks/steel',
+        C: '#forge:gems/diamond'
+    })
+})
