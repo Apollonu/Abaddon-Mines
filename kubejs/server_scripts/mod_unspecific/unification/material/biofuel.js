def60690dbd27348b4452c638149d183b908c4ff
Apollo <kingfrostalpha@gmail.com>
2025-11-17 00:42:28 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/unification/material/biofuel.js
+++ kubejs/server_scripts/mod_unspecific/unification/material/biofuel.js
@@ -22,23 +22,34 @@ ServerEvents.recipes(event => {
 
     //Custom Recipe Types
     let custom_recipe_types = [
-        {
-            'type': 'createaddition:liquid_burning',
-            'input': {
-                'fluid': unified_individual,
-                'amount': 1000
-            },
-            'burnTime': 24000,
-            'superheated': true
-        }
+        
     ]
 
     //Other Recipe Types
-    event.recipes.create.mixing(Fluid.of(unified_individual, 125), [
-        'minecraft:sugar',
-        'create:cinder_flour',
-        '2x createaddition:biomass'
-    ])
+    event.forEachRecipe({type: 'create:mixing'}, recipe => {
+        const output = recipe.json.get('results')
+        if (!output) return
+        output.forEach(type => {
+            const output_variant = type.get('fluid')?.asString
+            if (!output_variant) return
+            output_variant.includes('createaddition:bioethanol') && type.addProperty('fluid',
+                output_variant.replace('createaddition:bioethanol', unified_individual)
+            )
+        })
+        
+    })
+    event.forEachRecipe({type: 'createaddition:liquid_burning'}, recipe => {
+        const output = recipe.json.get('input')
+        if (!output) return
+        const output_variant = output.get('fluidTag')?.asString
+        if (!output_variant) return
+        if (output_variant.includes('forge:biofuel')) {
+            output.remove('fluidTag')
+            output.addProperty('fluid',
+                output_variant.replace('forge:biofuel', unified_individual)
+            )
+        }
+    })
 
     //Unify Items Function Push
     unify_items (
