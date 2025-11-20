59c8cbf16ba62126f08c104f2958ee5240c88f24
Apollo <kingfrostalpha@gmail.com>
2025-11-20 02:13:38 +0000

    Add files via upload

--- kubejs/server_scripts/master_files/master_unification.js
+++ kubejs/server_scripts/master_files/master_unification.js
@@ -2,7 +2,18 @@
 //priority: 4
 
 //Unify Items Function
-function unify_items (event, unified_individual, unified_variants, output_recipe_types, input_recipe_types, custom_recipe_types) {
+function unify_items (data) {
+
+	//Variables
+	let event = data.event
+	let unified_individual = data.unified_individual || []
+	let unified_variants = data.unified_variants || []
+	let output_recipe_types = data.output_recipe_types || []
+	let input_recipe_types = data.input_recipe_types || []
+	let json_unified_variants = data.json_unified_variants || []
+	let json_recipe_types = data.json_recipe_types || []
+	let fluid_unified_variants = data.fluid_unified_variants || []
+	let fluid_recipe_types = data.fluid_recipe_types || []
 
 	//Recipe Logic
 	unified_variants.forEach(variant => {
@@ -20,8 +31,61 @@ function unify_items (event, unified_individual, unified_variants, output_recipe
 		})
 	})
 
-	//Custom Recipe Logic
-	custom_recipe_types.forEach(individual => {
-		event.custom(individual)
-	})
+	//JSON Recipe Logic
+	if (unified_individual != null && json_unified_variants != null && json_recipe_types != null) {
+		json_unified_variants.forEach(variant => {
+			json_recipe_types.forEach(type => {
+				event.forEachRecipe({type: type}, recipe => {
+					let json = recipe.json.toString()
+					variant = variant.toString()
+					if (json.includes(variant)) {
+						console.log('/// JSON Recipe Start')
+						console.log('Variant: ' + variant)
+						console.log('Recipe: ' + json)
+
+						let unified_individual_string = '"item":"' + unified_individual + '"'
+						if (type.includes('ad_astra:compressing')) unified_individual_string = unified_individual_string.replace('item', 'id')
+						console.log('Unified Individual: ' + unified_individual_string)
+
+						json = json.replace(variant, unified_individual_string)
+
+						console.log('Altered Recipe: ' + json)
+						console.log('/// JSON Recipe End')
+
+						recipe.merge(JSON.parse(json))
+					}
+				})
+			})
+		})
+	}
+
+	//Fluid Recipe Logic
+	if (unified_individual != null && fluid_unified_variants != null && fluid_recipe_types != null) {
+		fluid_unified_variants.forEach(variant => {
+			fluid_recipe_types.forEach(type => {
+				event.forEachRecipe({type: type}, recipe => {
+					let json = recipe.json.toString()
+					variant = variant.toString()
+					if (json.includes(variant)) {
+						console.log('/// Fluid Recipe Start')
+						console.log('Variant: ' + variant)
+						console.log('Recipe: ' + json)
+
+						let unified_individual_string = '"fluid":"' + unified_individual + '"'
+						if (type.includes('createaddition:liquid_burning') && variant.includes('plantoil') && variant.includes('"input":{') == 0) return
+						if (type.includes('createaddition:liquid_burning')) unified_individual_string = '"input":{' + unified_individual_string
+						if (type.includes('pneumaticcraft:amadron')) unified_individual_string = unified_individual_string.replace('fluid', 'id')
+						console.log('Unified Individual: ' + unified_individual_string)
+
+						json = json.replace(variant, unified_individual_string)
+
+						console.log('Altered Recipe: ' + json)
+						console.log('/// Fluid Recipe End')
+
+						recipe.merge(JSON.parse(json))
+					}
+				})
+			})
+		})
+	}
 }
