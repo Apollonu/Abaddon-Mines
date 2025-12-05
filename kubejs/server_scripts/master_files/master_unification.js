c319f23284d4a00499bcea177c472f9db242ac39
Apollo <kingfrostalpha@gmail.com>
2025-12-05 15:51:36 +0000

    Add files via upload

--- kubejs/server_scripts/master_files/master_unification.js
+++ kubejs/server_scripts/master_files/master_unification.js
@@ -10,10 +10,9 @@ function unify_items (data) {
 	let unified_variants = data.unified_variants
 	let output_recipe_types = data.output_recipe_types
 	let input_recipe_types = data.input_recipe_types
+	let is_liquid = data.is_liquid
 	let json_unified_variants = data.json_unified_variants
 	let json_recipe_types = data.json_recipe_types
-	let fluid_unified_variants = data.fluid_unified_variants
-	let fluid_recipe_types = data.fluid_recipe_types
 
 	//Recipe Logic
 	if (unified_individual != null && unified_variants != null) {
@@ -45,54 +44,31 @@ function unify_items (data) {
 					let json = recipe.json.toString()
 					variant = variant.toString()
 					if (json.includes(variant)) {
+						let unified_individual_string
+						if (is_liquid == true) {
+							unified_individual_string = '"fluid":"' + unified_individual + '"'
+							if (type.includes('createaddition:liquid_burning') && variant.includes('plantoil') && variant.includes('"input":{') == 0) return
+							if (type.includes('createaddition:liquid_burning')) unified_individual_string = '"input":{' + unified_individual_string
+							if (type.includes('pneumaticcraft:amadron')) unified_individual_string = unified_individual_string.replace('fluid', 'id')
+						} else {
+							unified_individual_string = '"item":"' + unified_individual + '"'
+							if (type.includes('ad_astra:compressing')) unified_individual_string = unified_individual_string.replace('item', 'id')
+						}
+
 						/*
 						console.log('/// JSON Recipe Start')
 						console.log('Variant: ' + variant)
 						console.log('Recipe: ' + json)
+						console.log('Unified Individual: ' + unified_individual_string)
 						*/
 
-						let unified_individual_string = '"item":"' + unified_individual + '"'
-						if (type.includes('ad_astra:compressing')) unified_individual_string = unified_individual_string.replace('item', 'id')
-						//console.log('Unified Individual: ' + unified_individual_string)
-
 						json = json.replace(variant, unified_individual_string)
-
-						//console.log('Altered Recipe: ' + json)
-						//console.log('/// JSON Recipe End')
-
 						recipe.merge(JSON.parse(json))
-					}
-				})
-			})
-		})
-	}
 
-	//Fluid Recipe Logic
-	if (unified_individual != null && fluid_unified_variants != null && fluid_recipe_types != null) {
-		fluid_unified_variants.forEach(variant => {
-			fluid_recipe_types.forEach(type => {
-				event.forEachRecipe({type: type}, recipe => {
-					let json = recipe.json.toString()
-					variant = variant.toString()
-					if (json.includes(variant)) {
 						/*
-						console.log('/// Fluid Recipe Start')
-						console.log('Variant: ' + variant)
-						console.log('Recipe: ' + json)
+						console.log('Altered Recipe: ' + json)
+						console.log('/// Fluid Recipe End')
 						*/
-
-						let unified_individual_string = '"fluid":"' + unified_individual + '"'
-						if (type.includes('createaddition:liquid_burning') && variant.includes('plantoil') && variant.includes('"input":{') == 0) return
-						if (type.includes('createaddition:liquid_burning')) unified_individual_string = '"input":{' + unified_individual_string
-						if (type.includes('pneumaticcraft:amadron')) unified_individual_string = unified_individual_string.replace('fluid', 'id')
-						//console.log('Unified Individual: ' + unified_individual_string)
-
-						json = json.replace(variant, unified_individual_string)
-
-						//console.log('Altered Recipe: ' + json)
-						//console.log('/// Fluid Recipe End')
-
-						recipe.merge(JSON.parse(json))
 					}
 				})
 			})
