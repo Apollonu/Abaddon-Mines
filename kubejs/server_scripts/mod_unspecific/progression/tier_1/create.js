cb37e7b56d76c06c07dddfd6f71bc02759bfe408
Apollo <kingfrostalpha@gmail.com>
2025-12-21 03:01:34 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/progression/tier_1/create.js
+++ kubejs/server_scripts/mod_unspecific/progression/tier_1/create.js
@@ -6,6 +6,31 @@ ServerEvents.recipes(event => {
 	//Variables
 	let prefix = 'create:'
 
+	//Copper Valve Handle Recipe
+	event.remove({output: prefix + 'copper_valve_handle', input: '#forge:plates/copper'})
+	event.shaped(prefix + 'copper_valve_handle', ['ABA', ' A ', ' C '], {
+		A: '#forge:plates/copper',
+		B: '#forge:rods/iron',
+		C: prefix + 'andesite_alloy'
+	})
+
+	//Encased Fan Recipe
+	event.remove({output: prefix + 'encased_fan'})
+	event.shaped(prefix + 'encased_fan', [' A ', 'DB ', ' C '], {
+		A: prefix + 'shaft',
+		B: prefix + 'andesite_casing',
+		C: prefix + 'propeller',
+		D: '#forge:rods/iron'
+	})
+
+	//Hand Crank Recipe
+	event.remove({output: prefix + 'hand_crank'})
+	event.shaped(prefix + 'hand_crank', ['ACA', '  B'], {
+		A: '#minecraft:planks',
+		B: prefix + 'andesite_alloy',
+		C: '#forge:rods/iron'
+	})
+
 	//Mechanical Press Recipe
 	event.remove({output: prefix + 'mechanical_press'})
 	event.shaped(prefix + 'mechanical_press', [' A', ' B', ' C' ], {
@@ -22,13 +47,6 @@ ServerEvents.recipes(event => {
 		C: '#forge:storage_blocks/andesite_alloy'
 	})
 
-	//Whisk Recipe
-	event.remove({output: prefix + 'whisk'})
-	event.shaped(prefix + 'whisk', [' A ', 'BAB', 'BBB' ], {
-		A: '#forge:ingots/andesite_alloy',
-		B: '#forge:plates/zinc'
-	})
-
 	//Propeller Recipe
 	event.remove({output: prefix + 'propeller'})
 	event.shaped(prefix + 'propeller', [' A ', 'ABA', ' A '], {
@@ -36,12 +54,10 @@ ServerEvents.recipes(event => {
 		B: prefix + 'cogwheel'
 	})
 
-	//Encased Fan Recipe
-	event.remove({output: prefix + 'encased_fan'})
-	event.shaped(prefix + 'encased_fan', [' A ', 'DB ', ' C '], {
-		A: prefix + 'shaft',
-		B: prefix + 'andesite_casing',
-		C: prefix + 'propeller',
-		D: '#forge:rods/iron'
+	//Whisk Recipe
+	event.remove({output: prefix + 'whisk'})
+	event.shaped(prefix + 'whisk', [' A ', 'BAB', 'BBB' ], {
+		A: '#forge:ingots/andesite_alloy',
+		B: '#forge:plates/zinc'
 	})
 })
