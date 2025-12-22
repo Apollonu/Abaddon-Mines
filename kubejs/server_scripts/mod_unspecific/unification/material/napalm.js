a5919b4ae79093983f16bd3ee730a99daf71b0d9
Apollo <kingfrostalpha@gmail.com>
2025-12-22 02:57:31 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/unification/material/napalm.js
+++ kubejs/server_scripts/mod_unspecific/unification/material/napalm.js
@@ -1,6 +1,28 @@
 //All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
 //priority: 3
 
+//Fluid Recipe Unification
+ServerEvents.recipes(event => {
+    
+    //Variables
+    let data = {
+        event: event,
+        unified_individual: 'immersivepetroleum:napalm',
+        json_ids: [
+            'tfmg:filling/napalm_potato',
+            'tfmg:mixing/napalm' //This Recipe is Removed
+        ]
+    }
+
+    //Other Recipe Types
+    event.recipes.create.filling('tfmg:napalm_potato', [Fluid.of('immersivepetroleum:napalm', 250), 'minecraft:potato'])
+
+    //Unify Items Function Push
+    unify_items (
+        data
+    )
+})
+
 //Wire Recipe Unification
 ServerEvents.recipes(event => {
     
