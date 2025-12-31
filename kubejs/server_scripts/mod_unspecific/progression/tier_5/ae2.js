87b8bcfcbd56f78ccbed8f75a7dad54cc2772de6
Apollo <kingfrostalpha@gmail.com>
2025-12-31 06:13:19 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/progression/tier_5/ae2.js
+++ kubejs/server_scripts/mod_unspecific/progression/tier_5/ae2.js
@@ -3,6 +3,37 @@
 //Recipe Event
 ServerEvents.recipes(event => {
 
+    //Variables
+    let prefix = 'ae2:'
+
+    //Processor Types
+    let processor_types = [
+        'calculation',
+        'engineering',
+        'logic'
+    ].forEach(type => {
+        let input
+
+        if (type == 'calculation') input = '#forge:gems/certus_quartz'
+        if (type == 'engineering') input = '#forge:gems/diamond'
+        if (type == 'logic') input = '#forge:ingots/gold'
+
+        event.recipes.immersiveengineering.metal_press(
+            prefix + 'printed_' + type + '_processor',
+            input,
+            prefix + type + '_processor_press'
+        )
+        event.recipes.immersiveengineering.metal_press(
+            prefix + type + '_processor_press',
+            'refinedstorage:quartz_enriched_iron_block',
+            prefix + type + '_processor_press'
+        )
+        event.recipes.immersiveengineering.arc_furnace([prefix + type + '_processor'],
+            prefix + 'printed_' + type + '_processor',
+            [prefix + 'printed_silicon', '#forge:dusts/redstone', 'refinedstorage:processor_binding']
+        )
+    })
+
     //Machine Casing Recipe
     event.remove({output: 'refinedstorage:machine_casing'})
     event.shaped('refinedstorage:machine_casing', ['AAA', 'ACA', 'BDB' ], {
@@ -12,14 +43,21 @@ ServerEvents.recipes(event => {
         D: 'ae2:engineering_processor'
     })
 
+    //Printed Silicon Recipe
+    event.recipes.immersiveengineering.metal_press(
+        prefix + 'printed_silicon',
+        '#forge:silicon',
+        prefix + 'silicon_press'
+    )
+
     //Quartz Enriched Iron
     event.remove({output: 'refinedstorage:quartz_enriched_iron', input: '#forge:ingots/iron'})
     event.custom({
         'type': 'tconstruct:casting_table',
-        'cast': {'item': '#forge:dusts/certus_quartz'},
+        'cast': {'tag': 'forge:dusts/certus_quartz'},
         'cast_consumed': true,
         'cooling_time': 82,
-        'fluid': {'amount': 180, 'tag': 'forge:molten_iron'},
+        'fluid': {'amount': 90, 'tag': 'forge:molten_iron'},
         'result': 'refinedstorage:quartz_enriched_iron'
     })
 })
