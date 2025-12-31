1d9dd9ac2ec5cebc43ef596bbdc042fbf069f9f1
Apollo <kingfrostalpha@gmail.com>
2025-12-31 03:12:57 +0000

    Delete kubejs/server_scripts/mod_unspecific/progression directory

--- kubejs/server_scripts/mod_unspecific/progression/tier_0/string.js
+++
@@ -1,8 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-
-//Recipe Event
-ServerEvents.recipes(event => {
-
-    //Hemp Recipe Removal
-    event.remove({output: 'minecraft:string', input: 'immersiveengineering:hemp_fiber'})
-})
