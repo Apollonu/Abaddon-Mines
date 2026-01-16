aec1c11f365826bddfec6523ac8753640b7ea079
Apollo <kingfrostalpha@gmail.com>
2026-01-16 22:20:55 +0000

    Add files via upload

--- kubejs/server_scripts/mod_unspecific/progression/tier_none/backpacks.js
+++ kubejs/server_scripts/mod_unspecific/progression/tier_none/backpacks.js
@@ -25,22 +25,27 @@ ServerEvents.recipes(event => {
     //Iron Backpack Recipe
     event.remove({output: 'sophisticatedbackpacks:iron_backpack'})
 	event.recipes.create.sequenced_assembly(['sophisticatedbackpacks:iron_backpack'], 'sophisticatedbackpacks:copper_backpack', [
-		event.recipes.createDeploying('sophisticatedbackpacks:copper_backpack', ['sophisticatedbackpacks:copper_backpack', 'ironchest:iron_chest']),
+		event.recipes.createDeploying('sophisticatedbackpacks:copper_backpack', ['sophisticatedbackpacks:copper_backpack', '#forge:string']),
+        event.recipes.createDeploying('sophisticatedbackpacks:copper_backpack', ['sophisticatedbackpacks:copper_backpack', 'ironchest:iron_chest']),
+        event.recipes.createDeploying('sophisticatedbackpacks:copper_backpack', ['sophisticatedbackpacks:copper_backpack', '#forge:leather']),
 		event.recipes.createDeploying('sophisticatedbackpacks:copper_backpack', ['sophisticatedbackpacks:copper_backpack', '#forge:gears/compressed_iron'])
-	]).transitionalItem('sophisticatedbackpacks:copper_backpack').loops(2)
+	]).transitionalItem('sophisticatedbackpacks:copper_backpack').loops(1)
 
     //Gold Backpack Recipe
     event.remove({output: 'sophisticatedbackpacks:gold_backpack'})
 	event.recipes.create.sequenced_assembly(['sophisticatedbackpacks:gold_backpack'], 'sophisticatedbackpacks:iron_backpack', [
-		event.recipes.createDeploying('sophisticatedbackpacks:iron_backpack', ['sophisticatedbackpacks:iron_backpack', 'ironchest:gold_chest']),
+		event.recipes.createDeploying('sophisticatedbackpacks:iron_backpack', ['sophisticatedbackpacks:iron_backpack', '#minecraft:wool']),
+        event.recipes.createDeploying('sophisticatedbackpacks:iron_backpack', ['sophisticatedbackpacks:iron_backpack', 'ironchest:gold_chest']),
+        event.recipes.createDeploying('sophisticatedbackpacks:iron_backpack', ['sophisticatedbackpacks:iron_backpack', '#forge:leather']),
 		event.recipes.createDeploying('sophisticatedbackpacks:iron_backpack', ['sophisticatedbackpacks:iron_backpack', 'thermal:rf_coil'])
-	]).transitionalItem('sophisticatedbackpacks:iron_backpack').loops(2)
+	]).transitionalItem('sophisticatedbackpacks:iron_backpack').loops(1)
 
     //Diamond Backpack Recipe
     event.remove({output: 'sophisticatedbackpacks:diamond_backpack'})
 	event.recipes.create.sequenced_assembly(['sophisticatedbackpacks:diamond_backpack'], 'sophisticatedbackpacks:gold_backpack', [
-		event.recipes.createDeploying('sophisticatedbackpacks:gold_backpack', ['sophisticatedbackpacks:gold_backpack', 'ironchest:diamond_chest']),
+		event.recipes.createDeploying('sophisticatedbackpacks:gold_backpack', ['sophisticatedbackpacks:gold_backpack', '#thermal:rockwool']),
+        event.recipes.createDeploying('sophisticatedbackpacks:gold_backpack', ['sophisticatedbackpacks:gold_backpack', 'ironchest:diamond_chest']),
+        event.recipes.createDeploying('sophisticatedbackpacks:gold_backpack', ['sophisticatedbackpacks:gold_backpack', '#forge:leather']),
 		event.recipes.createDeploying('sophisticatedbackpacks:gold_backpack', ['sophisticatedbackpacks:gold_backpack', '#forge:circuits/basic'])
-	]).transitionalItem('sophisticatedbackpacks:gold_backpack').loops(2)
-	
+	]).transitionalItem('sophisticatedbackpacks:gold_backpack').loops(1)
 })
