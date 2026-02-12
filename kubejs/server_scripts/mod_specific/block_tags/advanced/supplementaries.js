e854dbd3b51a222e24d7d72037307eb3fc740f73
Apollo <kingfrostalpha@gmail.com>
2026-02-12 02:44:56 +0000

    Delete kubejs/server_scripts/mod_specific/block_tags/advanced directory

--- kubejs/server_scripts/mod_specific/block_tags/advanced/supplementaries.js
+++
@@ -1,9 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-//priority: 2
-
-//Block Tag Event
-ServerEvents.tags('block', event => {
-	
-	//Flint Block Tag
-	event.add('forge:storage_blocks/flint', 'supplementaries:flint_block')
-})
