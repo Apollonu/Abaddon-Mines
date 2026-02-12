2ce9eba3bf1fd81b2eeef184cbfbd773537b38fa
Apollo <kingfrostalpha@gmail.com>
2026-02-12 02:46:06 +0000

    Delete kubejs/server_scripts/mod_unspecific/block_tags directory

--- kubejs/server_scripts/mod_unspecific/block_tags/windmill.js
+++
@@ -1,8 +0,0 @@
-//All Rights Reserved to Apollo, this is not allowed to be used in public packs not released by Apollo.
-
-//Item Tag Event
-ServerEvents.tags('item', event => {
-
-    //Inventory Types
-    event.remove('create:windmill_sails', 'create:sail_frame')
-})
